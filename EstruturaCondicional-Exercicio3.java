d811c3588f246ad239561ea4af2b15065acac841
Carlos Eduardo Serpa Brito <61998196+CarlosEduardoSB@users.noreply.github.com>
2023-04-24 21:04:33 -0300

    Create EstruturaCondicional-Exercicio3.java

---
+++ EstruturaCondicional-Exercicio3.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int A, B;
+		
+		System.out.println("Digite o primeiro Número Inteiro:");
+		A = sc.nextInt();
+		System.out.println("Digite o segundo Número Inteiro:");
+		B = sc.nextInt();
+		
+		if (A % B == 0 || B % A == 0) {
+			System.out.println("Os valores são Múltiplos!");
+		}
+		
+		else {
+			System.out.println("Os valores não são Múltiplos!");
+		}
+		
+		sc.close();
+
+	}
+
+}
